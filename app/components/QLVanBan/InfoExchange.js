5d1efaa5fab53e82d167b8bf6ecbda5b16763e33
dangdong94 <ndd1994ptit@gmail.com>
2018-12-11 17:01:08 +0700

    up code 17:00 11/12/2017

--- app/components/QLVanBan/InfoExchange.js
+++ app/components/QLVanBan/InfoExchange.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Dimensions, TextInput, TouchableOpacity, Text, KeyboardAvoidingView } from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
+import { connect } from 'react-redux';
+import * as infoExchangeAction from "../../actions/infoExchange-actions";
+import * as rootActions from "../../actions/root-actions";
+import DefaultHeader from "../navigation/DefaultHeader";
+
 const { height, width } = Dimensions.get('window');
 
-export default class extends Component {
-    state = {}
+export class InfoExchange extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            flagLoad: true,
+            pageNo: "1",
+            pageRec: "10",
+            param: "",
+            listComment: [],
+        }
+    }
+
+    componentWillMount() {
+        this.props.dispatch(rootActions.controlProgress(false));
+        var idDocument = this.props.navigation.getParam('idDocument', 'NO-ID');
+        this.setState({
+            param: idDocument,
+        });
+        this.props.dispatch(infoExchangeAction.getListCommentAction(this.state.pageNo, this.state.pageRec, this.state.param));
+    }
+
+    componentWillReceiveProps() {
+        this.setState({
+            listComment: this.props.infoExchangeReducer.get('listComment'),
+        });
+    }
+
     render() {
+        let dataView;
+        if (this.state.listComment != null && this.state.listComment.length != 0) {
+            dataView = <FlatList
+                data={this.state.listComment}
+                renderItem={({ item, index }) => {
+                    return (
+                        <View style={{ flex: 1, flexDirection: 'column', alignItems: 'flex-end', paddingTop: 10, paddingRight: 4 }}>
+                            <Text style={{ color: '#999999', fontSize: 15 }}>{item.time}</Text>
+                            <Text style={{ color: 'black', fontSize: 15, backgroundColor: '#ffffff', padding: 7, borderRadius: 5 }}>{item.comment}</Text>
+                        </View>
+                    );
+                }}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        } else {
+            data = "";
+        }
         return (
             <View style={{ flex: 1 }}>
+
+                <DefaultHeader myTitle={this.props.navigation.getParam('title', '')} navigator={this.props.navigation} />
+
                 <View style={{ flex: 12, backgroundColor: '#D7D7D7' }}>
-                    <ScrollView >
-                        <View style={{ flex: 1, flexDirection: 'column', alignItems: 'flex-end', paddingTop: 10, paddingRight: 4 }}>
-                            <Text style={{ color: '#999999', fontSize: 15 }}>03/11/2018 20:54:10</Text>
-                            <Text style={{ color: 'black', fontSize: 15, backgroundColor: '#ffffff', padding: 7, borderRadius: 5 }}>test</Text>
-                        </View>
-                    </ScrollView>
+                    {dataView}
                 </View>
 
                 <View style={{ flex: 2 }}>
@@ -52,3 +97,12 @@ export default class extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        infoExchangeReducer: state.get('infoExchangeReducer'),
+        root: state.get('root'),
+    }
+}
+
+export default connect(mapStateToProps)(InfoExchange)
--- app/saga/infoExchange-saga.js
+++ app/saga/infoExchange-saga.js
@@ -2,23 +2,19 @@ import {call, put, take} from "redux-saga/effects";
 import * as actions from "../actions/action-types";
 import * as Api from "../api";
 import * as loginActions from "../actions/login-actions";
-import * as documentActions from "../actions/document-action";
+import * as infoExchangeActions from "../actions/infoExchange-actions";
 import * as rootActions from "../actions/root-actions";
 import apiUrl from "../network/apiUrl";
 import consts from "../const";
 
-function getListCommentURL(param) {
+function getListCommentURL(pageNo, pageRec, param) {
 
-    url = apiUrl.ROOT_URL + apiUrl.GET_DOC_WAIT_URL;
+    url = apiUrl.ROOT_URL + apiUrl.GET_LIST_COMMENT_URL + param + "/" + pageNo + "/" + pageRec + "/";
     console.log("url get waiting document:", url);
     console.log("param:", param);
     return fetch(url, {
     method: 'GET',
     headers: consts.BASE_HEADER,
-    body: JSON.stringify({
-      "param": param,
-
-    }),
   })    
   .then((response) => {
     return response.json();
@@ -28,25 +24,36 @@ function getListCommentURL(param) {
   }); 
 }
 
-function* getListComment(param) {
+function* getListComment(pageNo, pageRec, param) {
     try {
-      response = yield call(getListCommentURL, param);
-      console.log("wating data:", response);
+      response = yield call(getListCommentURL, pageNo, pageRec, param);
+      console.log("list comment:", response);
       if(typeof(response) != "undefined"  && typeof(response.status) != "undefined"){
         if (response.status.code == "0" && typeof(response.data) != "undefined") {
-          yield put(documentActions.setListWaitingDocumentSuccess(response.data));
+          yield put(infoExchangeActions.setListCommentSuccessAction(response.data));
           return response;
         } else {
-          yield put(documentActions.setListDocumentErrorAction(response.status.message));
+          yield put(infoExchangeActions.setListCommentErrorAction(response.status.message));
           return undefined;
         }
       }else{
-        yield put(documentActions.setListDocumentErrorAction("Không lấy được dữ liệu!"));
+        yield put(infoExchangeActions.setListCommentErrorAction("Không lấy được dữ liệu!"));
         return undefined;
       }
   
     } catch (error) {
       
-      yield put(documentActions.setListDocumentErrorAction(String(error)));
+      yield put(infoExchangeActions.setListCommentErrorAction(String(error)));
+    }
+  }
+
+  export function* infoExchangeFlow() {
+    while (true) {
+  
+      const {pageNo, pageRec, param} = yield take(actions.GET_LIST_COMMENT);
+      yield put(rootActions.controlProgress(true));
+      yield call(getListComment, pageNo, pageRec, param);
+      yield put(rootActions.controlProgress(false));
+  
     }
   }
